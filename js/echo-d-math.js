76761c8cd4a4aa1930bd75aa2765c2a4f927a6d9
KiraEatsMetal <kiraeatsmetal@gmail.com>
2026-05-29 18:55:21 -0700

    first image segmenting commit
    added clearer comments, made all canvases screen wide, added a few debug canvases
    
    added a bit of image processing and some code to detect the 9 input field boxes and draw a rectangle showing where they are detected (green) plus showing the targeted crop (red), added code to remove the boxes from the original image

--- js/echo-d-math.js
+++ js/echo-d-math.js
@@ -2,6 +2,7 @@ import scribe from './libraries/node_modules/scribe.js-ocr/scribe.js';
 //console.log(cv);
 
 function runFlowChart(epSeptal, epLateral, EeSeptal, EeLateral, averageEe, LAVI, TRVelocity, EA){
+    console.log("RUNNING FLOWCHART")
     let final = "ERROR";
 
     let stageOneMarkerCount = 0;
@@ -104,14 +105,14 @@ const dataLabelToHTMLIDTranslator = {
 
 /*==Image loading stuff==*/
 imageInputElement.addEventListener("change", async () => {
-    //exit if no files uploaded
-    if (!imageInputElement.files) return;
-    console.log(imageInputElement.files);
-    imageHolder.src = URL.createObjectURL(imageInputElement.files[0]);
+    if (!imageInputElement.files) return; //exit if no files uploaded
+    //console.log(imageInputElement.files);
+    imageHolder.src = URL.createObjectURL(imageInputElement.files[0]); //convert uploaded file to image source so opencv can read and process it
 
-    console.log("recieved image, starting scan");
+    console.log(":: recieved image, starting scan");
     outputTextArea.value = "Loading...";
 
+    //clear current data labels
     dataLabels.forEach((dataLabel) => {
         if (dataLabelToHTMLIDTranslator[dataLabel]) {
             document.getElementById(dataLabelToHTMLIDTranslator[dataLabel]).value = null;
@@ -120,43 +121,136 @@ imageInputElement.addEventListener("change", async () => {
 
     //scribeFile(imageInputElement.files)
 })
-/*==end of mage loading stuff==*/
+/*==end of image loading stuff==*/
 
 /*==OpenCV stuff==*/
 imageHolder.onload = async () => {
-    console.log("starting opencv processing :: ", imageHolder, imageHolder.width, imageHolder.height);
+    console.log()
+    console.log(":: starting opencv processing :: ", imageHolder, imageHolder.width, imageHolder.height);
     
-    //halve image resolution for better ocr results
-    imageHolder.width = imageHolder.width / 2;
-    console.log(imageHolder, imageHolder.width, imageHolder.height);
-    
-    let mat = cv.imread(imageHolder); //reads image from file to cv mat
-    //reset image resolution, otherwise subsequent uses have their resolution exponentially halved
-    imageHolder.width = imageHolder.width * 2;
+    imageHolder.width = imageHolder.width / 2; //halve image resolution for better ocr results
+    //console.log(imageHolder, imageHolder.width, imageHolder.height);
+    let imageInput = cv.imread(imageHolder); //reads image from file to cv mat
+    imageHolder.width = imageHolder.width * 2; //reset image resolution, otherwise subsequent uses have their resolution exponentially halved
 
-    cv.GaussianBlur(mat, mat, {width: 3, height: 3}, 0, 0); //gauss blur
-    cv.cvtColor(mat, mat, cv.COLOR_RGBA2GRAY); //grayscale
-    cv.normalize(mat, mat, 0, 255, cv.NORM_MINMAX); //normalize
-    cv.threshold(mat, mat, 128 + 18, 255, cv.THRESH_BINARY); //convert to black and white image, black/white cutoff is 50% (128) + testing number
-    /*
-    good test values:
-    16, 20, 18
-    */
-    
-    cv.imshow('canvasOutput', mat); //draw to canvas
+    //adjusting image for better ocr results
+    cv.GaussianBlur(imageInput, imageInput, {width: 3, height: 3}, 0, 0); //gauss blur
+    cv.cvtColor(imageInput, imageInput, cv.COLOR_RGBA2GRAY); //grayscale
+    cv.normalize(imageInput, imageInput, 0, 255, cv.NORM_MINMAX); //normalize
+    cv.threshold(imageInput, imageInput, 128 + 18, 255, cv.THRESH_BINARY_INV); //convert to black and white image, black/white cutoff is 50% (128) + testing number
+    //good test values: 16, 20, 18
 
+    //gap closing
+    cv.dilate(imageInput, imageInput, cv.getStructuringElement(cv.MORPH_RECT, new cv.Size(2, 2))); //thickens lines
+    cv.erode(imageInput, imageInput, cv.getStructuringElement(cv.MORPH_RECT, new cv.Size(2, 2))); //thins them
+    //doing these in that order closes gaps, reverse order removes small items. ORDER REVERSES IF IMAGE IS BW VS WB
+
+    //box detection adapted from https://towardsdatascience.com/checkbox-table-cell-detection-using-opencv-python-332c57d25171/
+    let line_min_width = 15
+    //object that defines a horizontal line
+    let kernal_h = cv.getStructuringElement(cv.MORPH_RECT, new cv.Size(line_min_width, 1)); //[[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]
+    //object that defines a vertical line
+    let kernal_v = cv.getStructuringElement(cv.MORPH_RECT, new cv.Size(1, line_min_width)); //[[1], [1], [1], [1], [1], [1], [1], [1], [1], [1], [1], [1], [1], [1], [1]]
+
+    //creates two bw images, one with detected vertical lines and one with detected horizontal lines
+    let img_bin_h = new cv.Mat();
+    cv.morphologyEx(imageInput, img_bin_h, cv.MORPH_OPEN, kernal_h);
+    let img_bin_v = new cv.Mat();
+    cv.morphologyEx(imageInput, img_bin_v, cv.MORPH_OPEN, kernal_v);
+
+    //merge both images
+    let img_bin_final = new cv.Mat();
+    cv.add(img_bin_h, img_bin_v, img_bin_final);
+    //remove unmerged
+    img_bin_h.delete(); //remove from memory
+    img_bin_v.delete(); //remove from memory
+
+    //invert image
+    cv.bitwise_not(img_bin_final, img_bin_final);
+    //fill holes in the boxes
+    cv.erode(img_bin_final, img_bin_final, cv.getStructuringElement(cv.MORPH_RECT, new cv.Size(5, 5))); //thickens lines
+    cv.dilate(img_bin_final, img_bin_final, cv.getStructuringElement(cv.MORPH_RECT, new cv.Size(5, 5))); //thins them
+    
+    //output variables for connectedComponentsWithStats
+    let labels = new cv.Mat();
+    let stats = new cv.Mat();
+    let centroids = new cv.Mat();
+
+    let numLabels = cv.connectedComponentsWithStats(img_bin_final, labels, stats, centroids); //detects boxes, outputs data to previous output variables
+
+    //area sorting variables
+    let areaDict = {}
+    let areaArray = []
+    //adapted from https://github.com/TechStark/opencv-js/pull/119
+    for (let i = 1; i < numLabels; i++) {
+        const area   = stats.intAt(i, cv.CC_STAT_AREA);
+        areaArray.push(area) //add every area to an array
+        areaDict[area] = i //and add each area and its index to a dict, so you can get the index from the area later
+    }
+
+    areaArray.sort(function(a, b){return b - a}) //sort array from largest to smallest
+    
+    cv.cvtColor(img_bin_final, img_bin_final, cv.COLOR_GRAY2RGB); //ungrayscale image so we can draw colour it later
+    cv.cvtColor(imageInput, imageInput, cv.COLOR_GRAY2RGB); //ungrayscale for same reason
+
+    //LOOK AT ME: what you need to do: make 9 canvases, output to them, toBlob each of them
+    //create 9 canvases and save references to them somewhere, preferably in an array
+    //give them an id so you can target and output to them, such as "box" + i
+    let outputCanvasArray = []
+
+    //target box is about 1/30 the area of the example images, so cut anything greater than 1/20th the image's area
+    //grab the first 9 boxes that are small enough
+    let areaThreshold = 20000; //replace this with a non-hardcoded value later, get image length and width, multiply them, get a portion of that area
+    let drawnBoxes = 0 //counter for boxes we draw/boxes that fit in the area threshold
+    for (let i = 0; i < areaArray.length; i++) {
+        if (drawnBoxes > 8) {console.log("drawn 9 target boxes"); break} //stop drawing boxes after you draw the 9 largest that fit in the threshold
+        const area = stats.intAt(areaDict[areaArray[i]], cv.CC_STAT_AREA);
+        //console.log(area, area < areaThreshold);
+        if (area < areaThreshold) {
+            const x      = stats.intAt(areaDict[areaArray[i]], cv.CC_STAT_LEFT);
+            const y      = stats.intAt(areaDict[areaArray[i]], cv.CC_STAT_TOP);
+            const width  = stats.intAt(areaDict[areaArray[i]], cv.CC_STAT_WIDTH);
+            const height = stats.intAt(areaDict[areaArray[i]], cv.CC_STAT_HEIGHT);
+            //xy is top right corner, so to get the label plus data box, use a rect from x-width, y to x+width, y+height
+            cv.rectangle(img_bin_final, new cv.Point(x-width,y), new cv.Point(x+width,y+height), new cv.Scalar(255, 0, 0), 1) //draws red box showing crop
+            cv.rectangle(img_bin_final, new cv.Point(x,y), new cv.Point(x+width,y+height), new cv.Scalar(0, 255, 0), 1) //draws green box showing detected box
+            cv.rectangle(imageInput, new cv.Point(x,y), new cv.Point(x+width,y+height), new cv.Scalar(0, 0, 0), 5) //removes box (fills with black) on image read by opencv
+            //make mat that is a crop of the image? either that or draw the entire inputImage again and crop the canvas?
+            //draw to canvas of index drawnBoxes
+            //cv.imshow('box' + drawnBoxes, imageInput)
+            drawnBoxes += 1
+        }
+    }
+    
+    //remove connectedComponentsWithStats output variables
+    labels.delete(); //remove from memory
+    stats.delete(); //remove from memory
+    centroids.delete(); //remove from memory
+
+    //draw to various canvases
+    cv.imshow('canvasOutput', imageInput); //draw to canvas
+    cv.imshow('debugCanvasOutputOne', img_bin_final); //draw to canvas
+    //cv.imshow('debugCanvasOutputTwo', img_bin_v); //draw to canvas
+    //cv.imshow('debugCanvasOutputThree', img_bin_h); //draw to canvas
+
+    let test = "start"
+    console.log("test: " + test)
+    
     //creates image file from canvas output, then feeds file to scribe
     outputCanvas.toBlob(function(blob) {
-        console.log("starting blob processing", blob);
+        console.log()
+        console.log(":: starting blob processing", blob);
         let file = new File([blob], 'canvasImage.png', { type: 'image/png' });
-        console.log("blob processing results:", file);
+        console.log(":: blob processing results:", file);
+        console.log("test: " + test)
         scribeFile([file])
     }, "image/png")
 
-    mat.delete(); //remove from memory
+    imageInput.delete(); //remove from memory
+    img_bin_final.delete(); //remove from memory
     imageHolder.src = null; // remove image source since we draw it in the canvas
     console.log("canvas: ", outputCanvas); //adjust width for viewing pleasure
-    console.log("finished opencv processing")
+    console.log(":: finished opencv processing")
 }
 /*==end of OpenCV stuff==*/
 //runs after you upload a file to the image input, specifically after that function feeds it to the image html element and it loads
@@ -169,13 +263,14 @@ async function scribeFile(filelist) {
     scribe.init({ ocr: true, ocrParams });
     
     //import and read files
-    console.log("scribing files")
+    console.log()
+    console.log(":: scribing files")
     await scribe.importFiles(filelist);
 
     await scribe.recognize(ocrParams.langs);
     const ocrExport = scribe.exportData('txt');
-    console.log("scribed files")
-    console.log(ocrExport);
+    console.log(":: scribed files, processing text")
+    //console.log(ocrExport);
 
     //string modification
     //get ocr export as string and remove cull characters, which are all useless
@@ -215,18 +310,20 @@ async function scribeFile(filelist) {
     //removing holes in array
     ocrStringArray = removeArrayHoles(ocrStringArray);
 
-    //add combined string to end of array
+    //combines array into a single string, helps find 'lost' label/data pairs that were split across array entries
     let combinedString = "";
     ocrStringArray.forEach((entry) => {
         combinedString += entry;
     })
     console.log("combined string: ", combinedString);
-    ocrStringArray.push(combinedString);
+    //pick one of these! one adds the string to the array, one just replaces the whole array with the string
+    //ocrStringArray.push(combinedString);
+    ocrStringArray = [combinedString]
     
     //display results
     outputTextArea.value = ocrStringArray.toString().replaceAll(",", "\n");
-    console.log(ocrStringArray);
-    console.log(outputTextArea.value);
+    //console.log(ocrStringArray);
+    //console.log(outputTextArea.value);
 
     dataLabels.forEach((dataLabel) => {
         if (dataLabelToHTMLIDTranslator[dataLabel]) {
@@ -255,6 +352,8 @@ async function scribeFile(filelist) {
             }
         })
     })
+
+    console.log(":: text processed")
 
     update();
 }
@@ -335,6 +434,7 @@ function update() {
     }
 
     console.log();
+    console.log(":: running flowchart");
     //get and assign for each variable
     let value
     for (const key of Object.keys(variableInput)) {
@@ -370,4 +470,5 @@ function update() {
     //show the result
     let output = document.getElementById("output");
     output.innerHTML = finalResult;
+    console.log(":: flowchart ran")
 }
